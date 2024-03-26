a476f37d8f7a16fb97b0d9912240ae90732d3960
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-03-26 23:35:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
 gsap.timeline({
   scrollTrigger: {
-    trigger: ".section-2",
-    pin: true,
-    start: "top top",
-    end: "+=" + window.innerHeight * 1.3,
-    scrub: 1,
-  }
-})
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".section-3",
-    pin: true,
-    start: "top top",
-    end: "+=" + window.innerHeight * 1.3,
-    scrub: 1,
-  }
-})
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".section-4",
-    pin: true,
-    start: "top top",
-    end: "+=" + window.innerHeight * 1.3,
-    scrub: 1,
-  }
-})
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".c-avatar",
-    start: "top top",
-    end: "bottom top",
-    scrub: 0,
-  }
-})
-  .to('#GK-intro', { opacity: 0 })
-gsap.timeline({
-  scrollTrigger: {
     trigger: ".c-roadmap",
     pin: false,
     start: "top bottom",
@@ -43,10 +7,8 @@ gsap.timeline({
     scrub: 1,
   }
 })
-  .to('.c-roadmap', { color: '#fff', backgroundColor: '#171010' })
-  .to('header', { color: '#fff' })
-  .to('.button-round', { color: '#171010', backgroundColor: '#fff', border: '1px solid #fff' 
-})
+.to('.button-round', { color: '#171010', backgroundColor: '#fff', border: '1px solid #fff' });
+
 window.addEventListener('load', () => {
   document.body.classList.remove('before-load');
 });
